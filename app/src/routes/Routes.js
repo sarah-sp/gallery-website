ac3e4db9d52f009c2d93cc1d1714c94ed281ab93
Sarah <sarah-sp@outlook.com>
2019-02-02 18:32:02 +0000

    Added more empty pages

--- app/src/routes/Routes.js
+++ app/src/routes/Routes.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {Switch} from 'react-router-dom';
 import AppliedRoute from './AppliedRoute';
-import AuthenticatedRoute from './AuthenticatedRoute';
-import Home from '../Home.js'
+import Home from '../pages/Home.js';
+import Collection from '../pages/Collection.js';
+import Visiting from '../pages/Visiting';
+import ContactUs from '../pages/ContactUs';
+import AboutUs from '../pages/AboutUs';
 
 export default ({childProps}) =>
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps}/>
+        <AppliedRoute path="/collection" exact component={Collection} props={childProps}/>
+        <AppliedRoute path="/visiting" exact component={Visiting} props={childProps}/>
+        <AppliedRoute path="/contacts" exact component={Visiting} props={ContactUs}/>
+        <AppliedRoute path="/about" exact component={AboutUs} props={childProps}/>
     </Switch>
