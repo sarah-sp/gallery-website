7843a8d509449287f441506d6994344c38187615
Sarah <sarah-sp@outlook.com>
2019-02-02 18:26:05 +0000

    Sidebar with items and logo

--- app/src/App.js
+++ app/src/App.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react';
 import Routes from './routes/Routes.js'
 import {Link, withRouter} from 'react-router-dom';
 import withStyles from "@material-ui/core/styles/withStyles";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import {main_theme} from "./Theme";
+import {sidebar_items} from "./Dummy";
+import Typography from "@material-ui/core/Typography/Typography";
+import logo from './assets/logo.png'
 
-let sidebarW = 30
-let contentW = 100 - sidebarW
 
-const styles = (theme) => ({
+let sidebarW = 25;
+let contentW = 100 - sidebarW;
+
+const styles = ({
     root: {
         display: "flex",
         flexWrap: "nowrap",
@@ -15,36 +21,59 @@ const styles = (theme) => ({
         minHeight: '100vh'
     },
     sidebar: {
-        width: sidebarW+'vw',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        justifyContent: 'space-evenly',
+        width: sidebarW + 'vw',
         position: 'fixed',
         height: '100vh',
-        backgroundColor: 'lightgray',
-
+        backgroundColor: main_theme.palette.primary.light,
+    },
+    sidebarItem: {
+        fontSize: '3vw',
+        color: main_theme.palette.primary.contrastText,
+        fontFamily: main_theme.palette.typography.fontFamily,
+        '&:hover': {
+            color: main_theme.palette.primary.main,
+            cursor: 'pointer'
+        }
+    },
+    image: {
+        width: '15vw',
+        height: '15vw'
     },
     content: {
-        marginLeft: sidebarW+'vw',
-        width: contentW+'vw',
-        backgroundColor: 'whitesmoke'
+        marginLeft: sidebarW + 'vw',
+        width: contentW + 'vw',
+        backgroundColor: main_theme.palette.primary.light
     }
-})
+});
 
 
 class App extends Component {
 
 
     render() {
-        const {classes} = this.props
-        let cProps = {}
+        const {classes} = this.props;
+        let cProps = {};
 
         return (
-            <div className={classes.root}>
-                <div className={classes.sidebar}>
-                    <b style={{fontSize: 150}}>SPA Template</b>
+            <MuiThemeProvider theme={main_theme}>
+                <div className={classes.root}>
+                    <div className={classes.sidebar}>
+                        <Link to={"/"}>
+                            <img className={classes.image} src={logo}/>
+                        </Link>
+                        {sidebar_items.map(item =>
+                            <Typography className={classes.sidebarItem}> {item} </Typography>
+                        )}
+                    </div>
+                    <div className={classes.content}>
+                        <Routes cProps/>
+                    </div>
                 </div>
-                <div className={classes.content}>
-                    <Routes cProps/>
-                </div>
-            </div>
+            </MuiThemeProvider>
         );
     }
 }
---
+++ app/src/Theme.js
@@ -0,0 +1,18 @@
+import {createMuiTheme} from "@material-ui/core";
+
+
+export const main_theme = createMuiTheme({
+
+    palette:{
+        primary:{
+            light: '#efefef',
+            main: '#9e9e9e',
+            dark: '#8d8d8d',
+            contrastText: '#111111',
+
+        },
+        typography: {
+            fontFamily: 'Bad Script'
+        }
+    }
+});
